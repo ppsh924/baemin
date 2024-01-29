604815ad9b496f1c1ac78a7113da1b1c04d6a7dc
ppsh924 <133199830+ppsh924@users.noreply.github.com>
2024-01-29 23:24:29 +0900

    Update FoodMapper.java

--- src/main/java/kr/co/baemin/mapper/FoodMapper.java
+++ src/main/java/kr/co/baemin/mapper/FoodMapper.java
@@ -32,30 +32,28 @@ public interface FoodMapper {
 	public FoodVo menu_put4(String m4);
 	public FoodVo menu_put5(String m5);
 	public ArrayList<CartVo> getCart(String userid);
-	
 	public int getCartFood(String fcode);
 	public void cartAllDel();
 	public ArrayList<FoodVo> menuallSearch(String fcode, String keyword);
 	public int chknum(String fcode, String userid);
-	
 	public MemberVo getMember(String userid);
-    public BaesongVo getJuso(String userid);
-    public void setBaesong(BaesongVo bvo);
-    public ArrayList<BaesongVo> baeview(String userid);
-    public void jusoadd_ok(BaesongVo bvo);
-    public BaesongVo baeup(String id);
-    public void baeup_ok(BaesongVo bvo);
-    public void gibon(String userid);  
-    public void baedel(String id);
-    public void gibonchg(String userid);
-    public void chg_ok(String cla, String id);
-    public CartVo cartv(String menu);
-    public void gumae_ok(GumaeVo gvo);
-    public void cartalldel(String userid);
-    public int getJcode(String jumuncode);
-    public ArrayList<HashMap<String,String>> getTotal(String jumuncode);
-    public void jumundel(String id);
-	 public ArrayList<ReviewVo> getReview(String fcode);
+        public BaesongVo getJuso(String userid);
+        public void setBaesong(BaesongVo bvo);
+        public ArrayList<BaesongVo> baeview(String userid);
+        public void jusoadd_ok(BaesongVo bvo);
+        public BaesongVo baeup(String id);
+        public void baeup_ok(BaesongVo bvo);
+        public void gibon(String userid);  
+        public void baedel(String id);
+        public void gibonchg(String userid);
+        public void chg_ok(String cla, String id);
+        public CartVo cartv(String menu);
+        public void gumae_ok(GumaeVo gvo);
+        public void cartalldel(String userid);
+        public int getJcode(String jumuncode);
+        public ArrayList<HashMap<String,String>> getTotal(String jumuncode);
+        public void jumundel(String id);
+	public ArrayList<ReviewVo> getReview(String fcode);
 	public ArrayList<AreviewVo> getAreview(String rid);
      
 }
